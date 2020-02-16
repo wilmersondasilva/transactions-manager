20d125ecc83e3d7d70e50017da802431246c98cb
Wilmerson da Silva <wilmersondasilva@gmail.com>
2020-02-16 02:22:54 -0300

    Creates TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h1>Transactions Manager</h1>
+    </header>
+</template>
+
+<style lang="stylus" scoped>
+    height = 50px
+
+    .header
+        width 100vw
+        height height
+        line-height height
+        background-color #fff
+        border-bottom 2px solid #e4e8f0
+        text-align center
+</style>
