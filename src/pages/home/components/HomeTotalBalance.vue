d4213995b91d67a58d85a6d69a0995cc2259db8a
Wilmerson da Silva <wilmersondasilva@gmail.com>
2020-02-18 11:23:51 -0300

    Creates HomeTotalBalance component

---
+++ src/pages/home/components/HomeTotalBalance.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="total-balance">
+        <span class="current-balance">Saldo atual:&nbsp;</span>
+        <span class="amount">{{ totalBalance | toMoney }}</span>
+    </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex'
+
+const { mapGetters } = createNamespacedHelpers('transactions')
+
+export default {
+    name: 'HomeTotalBalance',
+    computed: {
+        ...mapGetters(['totalBalance'])
+    }
+}
+</script>
