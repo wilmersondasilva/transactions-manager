b42fad7c86a53d1a197a821d4da0ce6015bb8727
Wilmerson da Silva <wilmersondasilva@gmail.com>
2020-02-18 11:23:27 -0300

    Creates HomeNewTransactionButton component

---
+++ src/pages/home/components/HomeNewTransactionButton.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <router-link to="/transaction/new">
+        <button class="new-transaction-button">Adicionar transação</button>
+    </router-link>
+</template>
+
+<style lang="stylus" scoped>
+.new-transaction-button
+    width 230px
+    height 40px
+    border-radius 4px
+    border 1px solid #eee
+    background-color #edeef2
+    font-size 1rem
+
+    &:focus
+        outline 0
+</style>
